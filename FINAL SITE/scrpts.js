cbff52a5874e67ed9cf60c410eb51eaa257d2cd0
macwoolleys <112663606+macwoolleys@users.noreply.github.com>
2022-12-13 19:58:33 -0500

    kkk

---
+++ FINAL SITE/scrpts.js
@@ -0,0 +1,19 @@
+
+$("#dark-btn").click(function(){
+
+    $(".box").toggleClass("dark");
+    $("body").toggleClass("dark");
+
+  });
+
+  $("#glow-btn").click(function(){
+
+    $(".box").toggleClass("glow");
+    
+  });
+
+  $("#spin-btn").click(function(){
+
+    $(".box").toggleClass("spin");
+    
+  });
